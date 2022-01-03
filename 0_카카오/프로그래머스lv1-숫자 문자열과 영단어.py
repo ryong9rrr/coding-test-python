974c6ddcff3889291f7f504e1198fbde85e4e296
ryong9rrr <tkddbs1644@gmail.com>
2022-01-03 19:26:58 +0900

    너무 쉬운 문제 삭제

--- 0_카카오/프로그래머스lv1-숫자 문자열과 영단어.py
+++ 0_카카오/프로그래머스lv1-숫자 문자열과 영단어.py
@@ -1,3 +1,4 @@
+# 테이블을 중심으로 탐색
 def solution(s):
     nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for i, num in enumerate(nums):
@@ -17,5 +18,36 @@ def solution(s):
 테스트 7 〉	통과 (0.02ms, 10.4MB)
 테스트 8 〉	통과 (0.02ms, 10.4MB)
 테스트 9 〉	통과 (0.02ms, 10.3MB)
+테스트 10 〉	통과 (0.02ms, 10.4MB)
+"""
+
+# 문자열을 중심으로 탐색
+def solution(s):
+    nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    result = ""
+    temp = ""
+    for char in s:
+        if char.isalpha():
+            temp += char
+        else:
+            result += char
+            
+        if temp in nums:
+            result += str(nums.index(temp))
+            temp = ""
+    
+    return int(result)
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.02ms, 10.4MB)
+테스트 2 〉	통과 (0.02ms, 10.4MB)
+테스트 3 〉	통과 (0.02ms, 10.4MB)
+테스트 4 〉	통과 (0.02ms, 10.4MB)
+테스트 5 〉	통과 (0.02ms, 10.4MB)
+테스트 6 〉	통과 (0.03ms, 10.4MB)
+테스트 7 〉	통과 (0.02ms, 10.3MB)
+테스트 8 〉	통과 (0.03ms, 10.4MB)
+테스트 9 〉	통과 (0.03ms, 10.4MB)
 테스트 10 〉	통과 (0.02ms, 10.4MB)
 """
--- 구현/백준18406-럭키 스트레이트.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-n = input()
-mid = len(n) // 2
-left = sum(map(int, n[:mid]))
-right = sum(map(int, n[mid:]))
-
-print("LUCKY") if left == right else print("READY")
