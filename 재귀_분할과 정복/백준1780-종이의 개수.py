e5e34ab1b4f4059684e3d4f2821aef1930f0e63d
ryong9rrr <tkddbs1644@gmail.com>
2022-01-05 19:32:18 +0900

    백준 분할정복 기본문제 다시풀기

--- 재귀_분할과 정복/백준1780-종이의 개수.py
+++ 재귀_분할과 정복/백준1780-종이의 개수.py
@@ -1,42 +1,102 @@
+# 행렬 자체를 슬라이싱해서 행렬을 인자로 넘겨주어 행렬에 직접적으로 접근하는 방법
 import sys
-input = lambda :sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10**6)
 
 n = int(input())
 matrix = []
 for _ in range(n):
-    matrix.append(input().split())
+    data = list(input().split())
+    matrix.append(data)
 
-number = {
+result = {
     "-1": 0,
     "0": 0,
-    "1": 0
+    "1": 0,
 }
 
-def dnc(n:int, x:int, y:int):
-    global number
+def checkMatrix(mat):
+    n = len(mat)
+    for i in range(n):
+        for j in range(n):
+            if mat[0][0] != mat[i][j]:
+                return False
+    return True
 
-    target = matrix[x][y]
-    for i in range(x, x + n):
-        for j in range(y, y + n):
-            if target != matrix[i][j]:
-                target = -2
-
-    if target == -2:
-        n = n // 3
-        dnc(n, x, y)
-        dnc(n, x, y + n)
-        dnc(n, x, y + 2 * n)
-        dnc(n, x + n, y)
-        dnc(n, x + n, y + n)
-        dnc(n, x + n, y + 2 * n)
-        dnc(n, x + 2 * n, y)
-        dnc(n, x + 2 * n, y + n)
-        dnc(n, x + 2 * n, y + 2 * n)
+def divide(mat):
+    global result
+    # 길이가 1인지 검사하지 않으면 시간초과가 날 수 있음.
+    if len(mat) == 1:
+        result[mat[0][0]] += 1
+        return
+    if checkMatrix(mat):
+        result[mat[0][0]] += 1
+        return
     else:
-        number[target] += 1
+        d = len(mat) // 3
+        divide([row[:d] for row in mat[:d]])
+        divide([row[d:2*d] for row in mat[:d]])
+        divide([row[2*d:3*d] for row in mat[:d]])
+        divide([row[:d] for row in mat[d:2*d]])
+        divide([row[d:2*d] for row in mat[d:2*d]])
+        divide([row[2*d:3*d] for row in mat[d:2*d]])
+        divide([row[:d] for row in mat[2*d:3*d]])
+        divide([row[d:2*d] for row in mat[2*d:3*d]])
+        divide([row[2*d:3*d] for row in mat[2*d:3*d]])
 
-dnc(n, 0, 0)
+divide(matrix)
 
-for value in number.values():
-    print(value)
+for ans in result.values():
+    print(ans)
+
+
+####################################################################
+# 첫번째 원소의 인덱스 값으로 행렬에 간접적으로 접근하는 방법
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+matrix = []
+for _ in range(n):
+    data = list(input().split())
+    matrix.append(data)
+
+result = {
+    "-1": 0,
+    "0": 0,
+    "1": 0,
+}
+
+def checkMatrix(n, i, j):
+    for x in range(i, i + n):
+        for y in range(j, j + n):
+            if matrix[i][j] != matrix[x][y]:
+                return False
+    return True
+
+def divide(n, i, j):
+    global result
+    # 길이가 1인지 검사하는 코드가 없어도 통과하지만, 이 코드 하나만으로 2600ms가 걸림. (없으면 5500ms)
+    if n == 1:
+        result[matrix[i][j]] += 1
+        return
+    if checkMatrix(n, i, j):
+        result[matrix[i][j]] += 1
+        return
+    else:
+        n //= 3
+        divide(n, i, j)
+        divide(n, i, j + n)
+        divide(n, i, j + 2 * n)
+        divide(n, i + n, j)
+        divide(n, i + n, j + n)
+        divide(n, i + n, j + 2 * n)
+        divide(n, i + 2 * n, j)
+        divide(n, i + 2 * n, j + n)
+        divide(n, i + 2 * n, j + 2 * n)
+
+divide(n, 0, 0)
+
+for ans in result.values():
+    print(ans)
--- 재귀_분할과 정복/백준1992-쿼드트리.py
+++ 재귀_분할과 정복/백준1992-쿼드트리.py
@@ -1,5 +1,57 @@
+# 행렬을 직접적으로 넘겨주는 방법 (내 방법)
 import sys
-input = lambda :sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+matrix = []
+for _ in range(n):
+    matrix.append(list(map(int, list(input()))))
+
+# 원소들의 전체 합을 리턴하는 함수
+def checkSum(mat):
+    _sum = 0
+    for row in mat:
+        _sum += sum(row)
+    return _sum
+
+# 결과값을 담을 전역변수
+result = ""
+def divide(mat:list, n):
+    global result
+    # 행렬이 1 x 1 형태가 되면 그 값을 리턴한다.
+    if n == 1:
+        result += str(mat[0][0])
+        return
+
+    _sum = checkSum(mat)
+    # 합이 크기와 같다면 모두 1로 구성되어있는 것
+    if _sum == n ** 2:
+        result += "1"
+        return
+    # 합이 0이라면 모두 0으로 구성되어있는 것
+    elif _sum == 0:
+        result += "0"
+        return
+    # 그렇지 않다면 4분할
+    else:
+        result += "("
+        mid = len(mat) // 2
+        divide([row[:mid] for row in mat[:mid]], mid)
+        divide([row[mid:] for row in mat[:mid]], mid)
+        divide([row[:mid] for row in mat[mid:]], mid)
+        divide([row[mid:] for row in mat[mid:]], mid)
+        result += ")"
+
+divide(matrix, n)
+
+print(result)
+
+################################################################
+
+# 재귀의 진수를 보여주는 우아한 풀이...
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10**6)
 
 n = int(input())
@@ -7,16 +59,17 @@ matrix = []
 for _ in range(n):
     matrix.append(input())
 
-def solve(n, i, j):
+def divide(n, i, j):
     if n == 1:
         return matrix[i][j]
-    a = solve(n//2, i, j)
-    b = solve(n//2, i, j + n//2)
-    c = solve(n//2, i + n//2, j)
-    d = solve(n//2, i + n//2, j + n//2)
-    if a == b and a == c and a == d and len(a) == 1:
+    mid = n // 2
+    a = divide(mid, i, j)
+    b = divide(mid, i, j + mid)
+    c = divide(mid, i + mid, j)
+    d = divide(mid, i + mid, j + mid)
+    if a == b and b == c and c == d and len(a) == 1:
         return a
     else:
         return "(" + a + b + c + d + ")"
 
-print(solve(n, 0, 0))
+print(divide(n, 0, 0))
