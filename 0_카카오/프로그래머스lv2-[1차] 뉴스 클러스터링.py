36b7db34af7ba1e201136df27eab987f28e46cbd
ryong9rrr <tkddbs1644@gmail.com>
2022-01-11 18:57:25 +0900

    KAKAO 뉴스 클러스터링, 거리두기 확인하기 다시풀기

--- 0_카카오/프로그래머스lv2-[1차] 뉴스 클러스터링.py
+++ 0_카카오/프로그래머스lv2-[1차] 뉴스 클러스터링.py
@@ -1,3 +1,6 @@
+# 아이디어, 집합, KAKAO
+
+# 처음 푼 풀이... 굳이 문자열을 나눌 때 재귀를 사용할 필요도 없고 정규표현식을 사용할 필요도 없었음.
 import sys
 sys.setrecursionlimit(10**6)
 import re
@@ -87,4 +90,49 @@ def solution(str1, str2):
 테스트 11 〉	통과 (0.87ms, 10.4MB)
 테스트 12 〉	통과 (0.14ms, 10.3MB)
 테스트 13 〉	통과 (0.33ms, 10.3MB)
+"""
+
+
+# 220111 풀이^^ 실력이 많이 늘었다.
+from collections import defaultdict
+
+def make_set(strings):
+    result = defaultdict(int)
+    for i in range(len(strings) - 1):
+        x = strings[i: i + 2]
+        if x.isalpha():
+            result[x.upper()] += 1
+    return result
+
+def solution(str1, str2):
+    a, b = make_set(str1), make_set(str2)
+    
+    if not a and not b:
+        return 65536
+    
+    all_keys = set(a.keys()) | set(b.keys())
+    intersection = 0
+    union = 0
+    for key in all_keys:
+        if key in a and key in b:
+            intersection += min(a[key], b[key])
+        union += max(a[key], b[key])
+    
+    return int((intersection / union) * 65536)
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.03ms, 10.2MB)
+테스트 2 〉	통과 (0.03ms, 10.3MB)
+테스트 3 〉	통과 (0.03ms, 10.3MB)
+테스트 4 〉	통과 (0.66ms, 10.4MB)
+테스트 5 〉	통과 (0.02ms, 10.4MB)
+테스트 6 〉	통과 (0.02ms, 10.3MB)
+테스트 7 〉	통과 (0.16ms, 10.3MB)
+테스트 8 〉	통과 (0.02ms, 10.3MB)
+테스트 9 〉	통과 (0.08ms, 10.3MB)
+테스트 10 〉	통과 (0.14ms, 10.3MB)
+테스트 11 〉	통과 (0.22ms, 10.4MB)
+테스트 12 〉	통과 (0.01ms, 10.3MB)
+테스트 13 〉	통과 (0.06ms, 10.3MB)
 """
--- 0_카카오/프로그래머스lv2-거리두기 확인하기.py
+++ 0_카카오/프로그래머스lv2-거리두기 확인하기.py
@@ -1,75 +1,68 @@
-# 가까이 앉아있는 사람들 리스트와 전체 리스트를 입력받아 결과를 반환합니다.
-def check_mht(arr:list, matrix:list)->bool:
-    n = len(arr)
+def check_mht(close, place):
+    n = len(close)
+    # 사람들의 조합
     for i in range(n):
-        for j in range(i+1, n):
-            [m1, n1] = arr[i]
-            [m2, n2] = arr[j]
-            # 맨해튼거리가
-            distance = abs(m2-m1) + abs(n2-n1)
-            # 1이라면 거리두기를 지키지 않은 것입니다.
+        for j in range(i + 1, n):
+            x1, y1 = close[i]
+            x2, y2 = close[j]
+            # 맨해튼거리 측정
+            distance = abs(x1 - x2) + abs(y1 - y2)
+            # 1이라면 거리두기를 지키지 않은 것
             if distance == 1:
                 return 0
-            # 2라면 파티션의 유무를 확인합니다.
-            # 테이블만 놓여져 있다면 거리두기를 지키지 않은 것입니다.
+            # 2라면 파티션 유무를 확인
             elif distance == 2:
-                if m2 == m1 and matrix[m1][n2-1] == "O":
+                if x1 == x2 and place[x1][y2 - 1] == "O":
                     return 0
-                elif n1 == n2 and matrix[m2-1][n1] == "O":
+                elif y1 == y2 and place[x2 - 1][y1] == "O":
                     return 0
-                else:
-                    x = matrix[m1][n2]
-                    y = matrix[m2][n1]
-                    if x == "O" or y == "O":
-                        return 0
-    # 모두 확인을 해서 이상이 없다면 거리두기를 잘 지킨 것입니다.
+                elif place[x1][y2] == "O" or place[x2][y1] == "O":
+                    return 0
     return 1
+    
 
 def solution(places):
     result = []
     for place in places:
-        # 가까이 앉아있는 사람들을 찾습니다.
+        # 가까이 앉은 사람들을 찾습니다.
         close = []
-        for m in range(5):
-            for n in range(5):
-                c = place[m][n]
-                if c == "P":
-                    close.append([m, n])
-        # 거리를 확인하여 결과를 반환합니다.
+        for n in range(5):
+            for m in range(5):
+                if place[n][m] == "P":
+                    close.append((n, m))
         result.append(check_mht(close, place))
-    
     return result
 
 """
 정확성  테스트
 테스트 1 〉	통과 (0.06ms, 10.3MB)
-테스트 2 〉	통과 (0.03ms, 10.3MB)
-테스트 3 〉	통과 (0.03ms, 10.4MB)
-테스트 4 〉	통과 (0.03ms, 10.3MB)
-테스트 5 〉	통과 (0.04ms, 10.3MB)
-테스트 6 〉	통과 (0.03ms, 10.3MB)
-테스트 7 〉	통과 (0.03ms, 10.3MB)
+테스트 2 〉	통과 (0.03ms, 10.2MB)
+테스트 3 〉	통과 (0.03ms, 10.3MB)
+테스트 4 〉	통과 (0.04ms, 10.3MB)
+테스트 5 〉	통과 (0.02ms, 10.3MB)
+테스트 6 〉	통과 (0.04ms, 10.2MB)
+테스트 7 〉	통과 (0.03ms, 10.2MB)
 테스트 8 〉	통과 (0.03ms, 10.3MB)
-테스트 9 〉	통과 (0.03ms, 10.3MB)
-테스트 10 〉	통과 (0.03ms, 10.3MB)
+테스트 9 〉	통과 (0.05ms, 10.3MB)
+테스트 10 〉	통과 (0.03ms, 10.2MB)
 테스트 11 〉	통과 (0.03ms, 10.3MB)
-테스트 12 〉	통과 (0.03ms, 10.2MB)
-테스트 13 〉	통과 (0.03ms, 10.3MB)
+테스트 12 〉	통과 (0.03ms, 10.3MB)
+테스트 13 〉	통과 (0.04ms, 10.3MB)
 테스트 14 〉	통과 (0.03ms, 10.3MB)
-테스트 15 〉	통과 (0.02ms, 10.3MB)
+테스트 15 〉	통과 (0.04ms, 10.2MB)
 테스트 16 〉	통과 (0.03ms, 10.3MB)
-테스트 17 〉	통과 (0.03ms, 10.3MB)
+테스트 17 〉	통과 (0.06ms, 10.3MB)
 테스트 18 〉	통과 (0.03ms, 10.3MB)
-테스트 19 〉	통과 (0.03ms, 10.3MB)
+테스트 19 〉	통과 (0.05ms, 10.3MB)
 테스트 20 〉	통과 (0.04ms, 10.3MB)
-테스트 21 〉	통과 (0.03ms, 10.2MB)
-테스트 22 〉	통과 (0.03ms, 10.3MB)
+테스트 21 〉	통과 (0.03ms, 10.3MB)
+테스트 22 〉	통과 (0.06ms, 10.3MB)
 테스트 23 〉	통과 (0.02ms, 10.3MB)
-테스트 24 〉	통과 (0.03ms, 10.3MB)
-테스트 25 〉	통과 (0.02ms, 10.3MB)
+테스트 24 〉	통과 (0.05ms, 10.3MB)
+테스트 25 〉	통과 (0.03ms, 10.2MB)
 테스트 26 〉	통과 (0.03ms, 10.3MB)
-테스트 27 〉	통과 (0.03ms, 10.3MB)
-테스트 28 〉	통과 (0.03ms, 10.3MB)
-테스트 29 〉	통과 (0.02ms, 10.3MB)
-테스트 30 〉	통과 (0.02ms, 10.3MB)
+테스트 27 〉	통과 (0.05ms, 10.3MB)
+테스트 28 〉	통과 (0.02ms, 10.3MB)
+테스트 29 〉	통과 (0.03ms, 10.3MB)
+테스트 30 〉	통과 (0.02ms, 10.4MB)
 """
