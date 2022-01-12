9659326fab4d85f871c056e43fe27fa621380469
ryong9rrr <tkddbs1644@gmail.com>
2022-01-12 17:51:21 +0900

    프로그래머스 lv2 멀쩡한 사각형, 소수찾기, 전화번호 목록

--- 수학_소수/프로그래머스lv1-소수찾기.py
+++ 수학_소수/프로그래머스lv1-소수찾기.py
@@ -1,24 +1,35 @@
 def solution(n):
-    sosu = [True] * (n+1)
-    for num in range(2, int(n**0.5)+1) :
-        if sosu[num] :
-            for i in range(num*num, n+1, num) :
-                sosu[i] = False
-    result = [x for x in range(2,n+1) if sosu[x]==True]
-    return len(result)
+    # 0 부터 n까지 소수의 배열을 만들어내는 함수
+    def makePrimeNumbers(n:int)->list:
+        prime_numbers = [True] * (n + 1)
+        prime_numbers[0:2] = [False, False]
+        for num in range(2, int(n * 0.5) + 1):
+            if prime_numbers[num]:
+                for i in range(num ** 2, n + 1, num):
+                    prime_numbers[i] = False
+        return prime_numbers
 
-"""js
-function solution(n) {
-  let sosu = Array.from({ length: n + 1 }, (v) => true);
+    prime = makePrimeNumbers(n)
 
-  for (var num = 2; num <= Math.sqrt(n); num++) {
-    if (sosu[num]) {
-      for (var i = num * num; i <= n; i += num) {
-        sosu[i] = false;
-      }
-    }
-  }
-  const result = sosu.filter((x) => x == true);
-  return result.length - 2;
-}
+    return prime[:n + 1].count(True)
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.4MB)
+테스트 2 〉	통과 (0.03ms, 10.3MB)
+테스트 3 〉	통과 (0.10ms, 10.2MB)
+테스트 4 〉	통과 (0.18ms, 10.4MB)
+테스트 5 〉	통과 (0.09ms, 10.2MB)
+테스트 6 〉	통과 (0.97ms, 10.3MB)
+테스트 7 〉	통과 (0.54ms, 10.3MB)
+테스트 8 〉	통과 (1.17ms, 10.2MB)
+테스트 9 〉	통과 (1.14ms, 10.3MB)
+테스트 10 〉	통과 (35.11ms, 14.3MB)
+테스트 11 〉	통과 (114.53ms, 23.5MB)
+테스트 12 〉	통과 (39.41ms, 14.7MB)
+효율성  테스트
+테스트 1 〉	통과 (128.41ms, 24.3MB)
+테스트 2 〉	통과 (121.79ms, 23.7MB)
+테스트 3 〉	통과 (125.39ms, 24.2MB)
+테스트 4 〉	통과 (125.85ms, 24.1MB)
 """
--- 수학_소수/프로그래머스lv2-소수 찾기.py
+++ 수학_소수/프로그래머스lv2-소수 찾기.py
@@ -1,40 +1,44 @@
-import itertools
+from itertools import permutations
+# 입력받은 n까지 원소가 True나 False인 소수배열을 만들어내는 함수
+def makePrimeNumbers(n:int)->list:
+        prime_numbers = [True] * (n + 1)
+        prime_numbers[0:2] = [False, False]
+        for num in range(2, int(n * 0.5) + 1):
+            if prime_numbers[num]:
+                for i in range(num ** 2, n + 1, num):
+                    prime_numbers[i] = False
+        return prime_numbers
+
 def solution(numbers):
-    n = int("".join(sorted(numbers, reverse=True)))
-    # 만들 수 있는 모든 수
-    p = set([])
-    for i in range(1, len(numbers) + 1):
-        for t in list(itertools.permutations(numbers, i)):
-            x = "".join(list(t))
-            p.add(int(x))
+    # 가장 큰 숫자만큼 소수 배열을 생성
+    n = int("".join(sorted(numbers, reverse = True)))
+    primeNumbers = makePrimeNumbers(n)
     
-    # 소수 리스트
-    sosu = [True] * (n + 1)
-    for i in range(2, int(n**0.5) + 1):
-        if sosu[i]:
-            for j in range(i**2, n + 1, i):
-                sosu[j] = False
+    # 만들어낼 수 있는 모든 숫자찾기 (순열)
+    # 중복 값은 제외하기 위해 set.add()
+    nums = set([])
+    for i in range(1, len(numbers) + 1):
+        for num in list(permutations(numbers, i)):
+            nums.add(int("".join(num)))
     
     result = 0
-    for n in p:
-        n = int(n)
-        if n > 1 and sosu[n]:
+    for num in nums:
+        if primeNumbers[num]:
             result += 1
-            
     return result
 
 """
 정확성  테스트
-테스트 1 〉	통과 (0.42ms, 10.5MB)
-테스트 2 〉	통과 (59.34ms, 15.5MB)
-테스트 3 〉	통과 (0.05ms, 10.5MB)
-테스트 4 〉	통과 (24.97ms, 12.6MB)
-테스트 5 〉	통과 (590.46ms, 36.6MB)
-테스트 6 〉	통과 (0.05ms, 10.5MB)
-테스트 7 〉	통과 (0.27ms, 10.4MB)
-테스트 8 〉	통과 (923.99ms, 53.4MB)
-테스트 9 〉	통과 (0.08ms, 10.5MB)
-테스트 10 〉	통과 (121.45ms, 18.1MB)
-테스트 11 〉	통과 (15.89ms, 11.1MB)
-테스트 12 〉	통과 (7.95ms, 10.5MB)
+테스트 1 〉	통과 (0.39ms, 10.5MB)
+테스트 2 〉	통과 (86.66ms, 15.5MB)
+테스트 3 〉	통과 (0.03ms, 10.4MB)
+테스트 4 〉	통과 (42.57ms, 12.6MB)
+테스트 5 〉	통과 (674.78ms, 36.7MB)
+테스트 6 〉	통과 (0.04ms, 10.4MB)
+테스트 7 〉	통과 (0.38ms, 10.4MB)
+테스트 8 〉	통과 (1104.28ms, 53.4MB)
+테스트 9 〉	통과 (0.10ms, 10.4MB)
+테스트 10 〉	통과 (128.07ms, 18.1MB)
+테스트 11 〉	통과 (11.89ms, 10.9MB)
+테스트 12 〉	통과 (6.06ms, 10.5MB)
 """
