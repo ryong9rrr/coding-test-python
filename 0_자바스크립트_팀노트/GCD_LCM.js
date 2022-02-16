318b6b24be42ce98dd9a107eccaa469bfc6120d2
ryong9rrr <tkddbs1644@gmail.com>
2022-02-16 17:48:43 +0900

    gcd_lcm 자바스크립트 코드, 프로그래머스 lv2 피보나치수, 최소공배수, h-index

---
+++ 0_자바스크립트_팀노트/GCD_LCM.js
@@ -0,0 +1,19 @@
+// 최대공약수
+// a: number, b: number => number
+function GCD(a, b) {
+  if (a % b === 0) return b;
+  return GCD(b, a % b);
+}
+
+let gcd = GCD(15, 20); // 5
+
+// 최소공배수
+function LCM(a, b) {
+  function GCD(a, b) {
+    if (a % b === 0) return b;
+    return GCD(b, a % b);
+  }
+
+  const gcd = GCD(a, b);
+  return (a * b) / gcd;
+}
