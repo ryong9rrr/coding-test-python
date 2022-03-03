1010e3d879ec56d17143d1b48bbd250c06cb5f7a
ryong9rrr <tkddbs1644@gmail.com>
2022-03-03 19:10:32 +0900

    프로그래머스 lv2 연습문제 최댓값과 최솟값(매우 쉬움)

---
+++ 배열/프로그래머스lv2-최댓값과 최솟값.js
@@ -0,0 +1,48 @@
+/*
+python, 풀이 시간은 둘 다 동일
+
+# 리스트 컴프리헨션
+def solution(s):
+    numbers = [int(number) for number in s.split(" ")]
+    return f"{min(numbers)} {max(numbers)}"
+
+# map()
+def solution(s):
+    numbers = list(map(int, s.split(" ")))
+    return f"{min(numbers)} {max(numbers)}"
+
+정확성  테스트
+테스트 1 〉	통과 (0.03ms, 10.4MB)
+테스트 2 〉	통과 (0.04ms, 10.3MB)
+테스트 3 〉	통과 (0.02ms, 10.3MB)
+테스트 4 〉	통과 (0.03ms, 10.5MB)
+테스트 5 〉	통과 (0.03ms, 10.4MB)
+테스트 6 〉	통과 (0.03ms, 10.4MB)
+테스트 7 〉	통과 (0.02ms, 10.3MB)
+테스트 8 〉	통과 (0.03ms, 10.3MB)
+테스트 9 〉	통과 (0.03ms, 10.3MB)
+테스트 10 〉	통과 (0.04ms, 10.2MB)
+테스트 11 〉	통과 (0.03ms, 10.3MB)
+테스트 12 〉	통과 (0.04ms, 10.4MB)
+*/
+
+function solution(s) {
+  const numbers = s.split(" ").map((str) => Number(str));
+  return Math.min(...numbers) + " " + Math.max(...numbers);
+}
+
+/*
+정확성  테스트
+테스트 1 〉	통과 (0.08ms, 30.2MB)
+테스트 2 〉	통과 (0.09ms, 30.1MB)
+테스트 3 〉	통과 (0.06ms, 30.1MB)
+테스트 4 〉	통과 (0.07ms, 30.1MB)
+테스트 5 〉	통과 (0.26ms, 30.1MB)
+테스트 6 〉	통과 (0.10ms, 30.2MB)
+테스트 7 〉	통과 (0.06ms, 30.2MB)
+테스트 8 〉	통과 (0.10ms, 30.2MB)
+테스트 9 〉	통과 (0.07ms, 30.2MB)
+테스트 10 〉	통과 (0.08ms, 30.2MB)
+테스트 11 〉	통과 (0.06ms, 30.2MB)
+테스트 12 〉	통과 (0.08ms, 30.2MB)
+*/
