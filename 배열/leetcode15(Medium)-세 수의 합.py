1542b68b33f832c628bfa479d69d5e068f442fc0
ryong9rrr <tkddbs1644@gmail.com>
2022-03-10 23:38:29 +0900

    파이썬 알고리즘 인터뷰 배열 8~9 JS로 풀기

--- 배열/leetcode15(Medium)-세 수의 합.py
+++ 배열/leetcode15(Medium)-세 수의 합.py
@@ -1,4 +1,49 @@
-# 투포인터 // 720ms (브루트포스 타임아웃)
+# 브루트포스 // 타임아웃
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        result = []
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                for k in range(j + 1, len(nums)):
+                    if k > j + 1 and nums[k] == nums[k - 1]:
+                        continue
+                    
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        result.append([nums[i], nums[j], nums[k]])
+        
+        return result
+"""js
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = []
+    
+    for (let i = 0; i < nums.length - 2 ; i++){
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        
+        for (let j = i + 1; j < nums.length - 1; j++){
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            
+            for (let k = j + 1; k < nums.length; k++){
+                if (k > j + 1 && nums[k] === nums[k - 1]) continue;
+                
+                if (nums[i] + nums[j] + nums[k] === 0) {
+                    result.push([nums[i], nums[j], nums[k]])
+                }
+            }
+        }
+    }
+    return result
+};
+"""
+
+# 투포인터 // 720ms
 class Solution(object):
     def threeSum(self, nums):
         """
@@ -33,3 +78,28 @@ class Solution(object):
                     right -= 1
                     
         return result
+"""
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = []
+    
+    for (let i = 0; i < nums.length - 2; i++){
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right){
+            const _sum = nums[i] + nums[left] + nums[right];
+            if (_sum < 0) left++;
+            else if (_sum > 0) right--;
+            else {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            }
+        }
+    }
+    return result
+};
+"""
--- 자료구조_스택/leetcode42(Hard)-빗물 트래핑.py
+++ 자료구조_스택/leetcode42(Hard)-빗물 트래핑.py
@@ -72,7 +72,7 @@ class Solution(object):
             while stack and height[i] > height[stack[-1]]:
                 top = stack.pop()
                 
-                if not len(stack):
+                if not stack:
                     break
                     
                 distance = i - stack[-1] - 1
@@ -82,3 +82,26 @@ class Solution(object):
             stack.append(i)
             
         return volume
+
+"""js
+var trap = function(height) {
+    const stack = [];
+    let volume = 0;
+    
+    for (let i = 0; i < height.length; i++){
+        
+        while (stack.length > 0 && height[i] > height[stack[stack.length - 1]]){
+            const top = stack.pop();
+            
+            if (stack.length === 0) break;
+            
+            const distance = i - stack[stack.length - 1] - 1;
+            const waters = Math.min(height[i], height[stack[stack.length - 1]]) - height[top];
+            volume += distance * waters
+        }
+        stack.push(i)
+    }
+    
+    return volume
+};
+"""
