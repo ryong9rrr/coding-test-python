462bdf86ce3d5ffebb507799663baa46e528c346
yong <tkddbs1644@gmail.com>
2022-07-10 17:34:55 +0900

    프로그래머스 Lv1: 숫자 문자열과 영단어, 키패드 누르기

--- 1_카카오/프로그래머스lv1-키패드 누르기.py
+++ 1_카카오/프로그래머스lv1-키패드 누르기.py
@@ -147,3 +147,175 @@ def solution(numbers, hand):
 테스트 19 〉	통과 (0.36ms, 10.3MB)
 테스트 20 〉	통과 (0.32ms, 10.4MB)
 """
+
+# 2022년 7월 다시 푼 풀이, 전체적으로 더 직관적이고 담백해진듯
+PHONE = {
+        "1": [0, 0],
+        "2": [0, 1],
+        "3": [0, 2],
+        "4": [1, 0],
+        "5": [1, 1],
+        "6": [1, 2],
+        "7": [2, 0],
+        "8": [2, 1],
+        "9": [2, 2],
+        "*": [3, 0],
+        "0": [3, 1],
+        "#": [3, 2],
+    }
+
+def get_distance(number, current_hand):
+    [x1, y1] = PHONE[str(number)]
+    [x2, y2] = current_hand
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def solution(numbers, hand):
+    
+    current_left = PHONE["*"]
+    current_right = PHONE["#"]
+    result = []
+    
+    for number in numbers:
+        #1. 숫자가 1, 4, 7이면 바로 "L"을 반환하고 왼손 위치를 옮긴다.
+        if number == 1 or number == 4 or number == 7:
+            current_left = PHONE[str(number)]
+            result.append("L")
+            continue
+        
+        #2. 숫자가 3, 6, 9면 바로 "R"을 반환하고 오른손 위치를 옮긴다.
+        if number == 3 or number == 6 or number == 9:
+            current_right = PHONE[str(number)]
+            result.append("R")
+            continue
+            
+        #3. 거리를 비교한다. 만약 거리가 같다면 주 손을 옮긴다.
+        left_distance = get_distance(number, current_left)
+        right_distance = get_distance(number, current_right)
+        
+        if (left_distance < right_distance):
+            current_left = PHONE[str(number)]
+            result.append("L")
+        elif (left_distance > right_distance):
+            current_right = PHONE[str(number)]
+            result.append("R")
+        else:
+            if hand == "left":
+                current_left = PHONE[str(number)]
+                result.append("L")
+            else:
+                current_right = PHONE[str(number)]
+                result.append("R")
+        
+    return "".join(result)
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.00ms, 10.3MB)
+테스트 2 〉	통과 (0.01ms, 10.4MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.00ms, 10.3MB)
+테스트 5 〉	통과 (0.01ms, 10.4MB)
+테스트 6 〉	통과 (0.01ms, 10.4MB)
+테스트 7 〉	통과 (0.01ms, 10.3MB)
+테스트 8 〉	통과 (0.02ms, 10.3MB)
+테스트 9 〉	통과 (0.02ms, 10.4MB)
+테스트 10 〉	통과 (0.02ms, 10.3MB)
+테스트 11 〉	통과 (0.04ms, 10.2MB)
+테스트 12 〉	통과 (0.03ms, 10.2MB)
+테스트 13 〉	통과 (0.06ms, 10.4MB)
+테스트 14 〉	통과 (0.13ms, 10.2MB)
+테스트 15 〉	통과 (0.35ms, 10.2MB)
+테스트 16 〉	통과 (0.32ms, 10.3MB)
+테스트 17 〉	통과 (0.67ms, 10.3MB)
+테스트 18 〉	통과 (0.58ms, 10.3MB)
+테스트 19 〉	통과 (0.66ms, 10.3MB)
+테스트 20 〉	통과 (0.62ms, 10.3MB)
+"""
+
+"""
+js
+
+const PHONE = {
+        "1": [0, 0],
+        "2": [0, 1],
+        "3": [0, 2],
+        "4": [1, 0],
+        "5": [1, 1],
+        "6": [1, 2],
+        "7": [2, 0],
+        "8": [2, 1],
+        "9": [2, 2],
+        "*": [3, 0],
+        "0": [3, 1],
+        "#": [3, 2],
+    }
+
+const getDistance = (number, currentHand) => {
+    const [x1, y1] = PHONE[String(number)];
+    const [x2, y2] = currentHand;
+    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+}
+
+function solution(numbers, hand) {
+    let currentLeft = PHONE["*"];
+    let currentRight = PHONE["#"];
+    
+    const changeHand = (number, hand) => {
+        if (hand === "L") {
+            currentLeft = PHONE[String(number)]
+            return "L"
+        }
+        
+        if (hand === "R") {
+            currentRight = PHONE[String(number)]
+            return "R"
+        }
+    }
+    
+    return numbers.map(number => {
+        if (number === 1 || number === 4 || number === 7) {
+            return changeHand(number, "L")
+        }
+        
+        if (number === 3 || number === 6 || number === 9) {
+            return changeHand(number, "R")
+        }
+        
+        const leftDistance = getDistance(number, currentLeft);
+        const rightDistance = getDistance(number, currentRight);
+        
+        if (leftDistance < rightDistance) {
+            return changeHand(number, "L")
+        } else if (leftDistance > rightDistance) {
+            return changeHand(number, "R")
+        } else {
+            return hand === "left" 
+                ? changeHand(number, "L")
+                : changeHand(number, "R")
+        }
+        
+    }).join("")
+}
+
+정확성  테스트
+테스트 1 〉	통과 (0.15ms, 29.7MB)
+테스트 2 〉	통과 (0.18ms, 30.2MB)
+테스트 3 〉	통과 (0.18ms, 30.2MB)
+테스트 4 〉	통과 (0.13ms, 29.9MB)
+테스트 5 〉	통과 (0.17ms, 30.2MB)
+테스트 6 〉	통과 (0.17ms, 30.1MB)
+테스트 7 〉	통과 (0.18ms, 30.2MB)
+테스트 8 〉	통과 (0.20ms, 29.7MB)
+테스트 9 〉	통과 (0.19ms, 29.9MB)
+테스트 10 〉	통과 (0.19ms, 29.6MB)
+테스트 11 〉	통과 (0.22ms, 30MB)
+테스트 12 〉	통과 (0.25ms, 30.2MB)
+테스트 13 〉	통과 (0.23ms, 30.1MB)
+테스트 14 〉	통과 (0.31ms, 30MB)
+테스트 15 〉	통과 (0.51ms, 30MB)
+테스트 16 〉	통과 (0.48ms, 30.1MB)
+테스트 17 〉	통과 (0.78ms, 30.1MB)
+테스트 18 〉	통과 (0.72ms, 30.2MB)
+테스트 19 〉	통과 (0.79ms, 30MB)
+테스트 20 〉	통과 (0.81ms, 30MB)
+"""
