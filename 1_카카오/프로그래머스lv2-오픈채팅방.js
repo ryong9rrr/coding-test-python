00ca6e61ec3dc88d983587227114c0c56c6fd210
yong <tkddbs1644@gmail.com>
2022-07-30 02:20:00 +0900

    프로그래머스Lv2: 카카오 - 오픈채팅방

--- 1_카카오/프로그래머스lv2-오픈채팅방.js
+++ 1_카카오/프로그래머스lv2-오픈채팅방.js
@@ -56,3 +56,61 @@ function solution(record) {
 // 테스트 30 〉	통과 (205.28ms, 88MB)
 // 테스트 31 〉	통과 (218.63ms, 107MB)
 // 테스트 32 〉	통과 (158.15ms, 96.4MB)
+
+// 22년 7월 풀이, 전체적인 방식은 예전과 비슷하지만 좀 더 깔끔해진듯.
+const MESSAGE_MAP = {
+  Enter: '님이 들어왔습니다.',
+  Leave: '님이 나갔습니다.',
+}
+
+function solution(record) {
+  const userTable = {}
+  const userLog = []
+
+  for (const command of record) {
+    const [action, userId, username] = command.split(' ')
+    if (action === 'Enter' || action === 'Change') {
+      userTable[userId] = username
+    }
+    if (action !== 'Change') {
+      userLog.push([action, userId])
+    }
+  }
+
+  return userLog.map(([action, userId]) => {
+    return `${userTable[userId]}${MESSAGE_MAP[action]}`
+  })
+}
+// 정확성  테스트
+// 테스트 1 〉	통과 (0.38ms, 30MB)
+// 테스트 2 〉	통과 (0.15ms, 29.9MB)
+// 테스트 3 〉	통과 (0.42ms, 30MB)
+// 테스트 4 〉	통과 (0.39ms, 30MB)
+// 테스트 5 〉	통과 (1.22ms, 30.1MB)
+// 테스트 6 〉	통과 (1.50ms, 30.1MB)
+// 테스트 7 〉	통과 (2.17ms, 30.1MB)
+// 테스트 8 〉	통과 (1.49ms, 30MB)
+// 테스트 9 〉	통과 (1.64ms, 29.8MB)
+// 테스트 10 〉	통과 (1.49ms, 30.2MB)
+// 테스트 11 〉	통과 (0.99ms, 29.8MB)
+// 테스트 12 〉	통과 (1.01ms, 30MB)
+// 테스트 13 〉	통과 (1.53ms, 30MB)
+// 테스트 14 〉	통과 (1.71ms, 30.1MB)
+// 테스트 15 〉	통과 (0.29ms, 30MB)
+// 테스트 16 〉	통과 (0.33ms, 30MB)
+// 테스트 17 〉	통과 (0.27ms, 29.7MB)
+// 테스트 18 〉	통과 (0.62ms, 29.9MB)
+// 테스트 19 〉	통과 (1.37ms, 30MB)
+// 테스트 20 〉	통과 (1.92ms, 29.8MB)
+// 테스트 21 〉	통과 (1.79ms, 30.1MB)
+// 테스트 22 〉	통과 (1.40ms, 29.9MB)
+// 테스트 23 〉	통과 (2.38ms, 30.1MB)
+// 테스트 24 〉	통과 (2.37ms, 30MB)
+// 테스트 25 〉	통과 (208.27ms, 78.2MB)
+// 테스트 26 〉	통과 (194.32ms, 91.8MB)
+// 테스트 27 〉	통과 (195.63ms, 99.9MB)
+// 테스트 28 〉	통과 (190.05ms, 102MB)
+// 테스트 29 〉	통과 (179.98ms, 102MB)
+// 테스트 30 〉	통과 (126.46ms, 82.6MB)
+// 테스트 31 〉	통과 (209.87ms, 104MB)
+// 테스트 32 〉	통과 (145.65ms, 94.7MB)
--- 1_카카오/프로그래머스lv2-오픈채팅방.py
+++ 1_카카오/프로그래머스lv2-오픈채팅방.py
@@ -185,3 +185,66 @@ def solution(record):
 테스트 31 〉	통과 (105.84ms, 56.9MB)
 테스트 32 〉	통과 (77.19ms, 52.8MB)
 """
+
+
+# 22년 7월 풀이 // 코드가 더 나아진듯 하다
+from collections import defaultdict
+
+MESSAGE_MAP = {
+    "Enter": "님이 들어왔습니다.",
+    "Leave": "님이 나갔습니다."
+}
+
+def solution(record):
+    user_table = defaultdict(str)
+    user_log = []
+    
+    for command in record:
+        commands = command.split(" ")
+        action, user_id = commands[0], commands[1]
+        if action == "Enter" or action == "Change":
+            username = commands[2]
+            user_table[user_id] = username
+        if not action == "Change":
+            user_log.append([action, user_id])
+    
+    return [f"{user_table[user_id]}{MESSAGE_MAP[action]}"
+            for action, user_id
+            in user_log]
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 9.95MB)
+테스트 2 〉	통과 (0.01ms, 10.1MB)
+테스트 3 〉	통과 (0.03ms, 10.1MB)
+테스트 4 〉	통과 (0.06ms, 10.3MB)
+테스트 5 〉	통과 (0.59ms, 10.3MB)
+테스트 6 〉	통과 (0.64ms, 10.5MB)
+테스트 7 〉	통과 (0.54ms, 10.2MB)
+테스트 8 〉	통과 (1.15ms, 10.3MB)
+테스트 9 〉	통과 (0.75ms, 10.3MB)
+테스트 10 〉	통과 (0.64ms, 10.3MB)
+테스트 11 〉	통과 (0.38ms, 10.1MB)
+테스트 12 〉	통과 (0.39ms, 10.1MB)
+테스트 13 〉	통과 (1.05ms, 10.3MB)
+테스트 14 〉	통과 (0.72ms, 10.4MB)
+테스트 15 〉	통과 (0.01ms, 10.1MB)
+테스트 16 〉	통과 (0.01ms, 10MB)
+테스트 17 〉	통과 (0.07ms, 10MB)
+테스트 18 〉	통과 (0.10ms, 10.1MB)
+테스트 19 〉	통과 (0.92ms, 10.4MB)
+테스트 20 〉	통과 (0.51ms, 10.1MB)
+테스트 21 〉	통과 (0.53ms, 10MB)
+테스트 22 〉	통과 (0.50ms, 10.1MB)
+테스트 23 〉	통과 (0.67ms, 10.4MB)
+테스트 24 〉	통과 (0.75ms, 10.2MB)
+테스트 25 〉	통과 (99.57ms, 45MB)
+테스트 26 〉	통과 (148.61ms, 49MB)
+테스트 27 〉	통과 (108.84ms, 52.8MB)
+테스트 28 〉	통과 (131.95ms, 54.8MB)
+테스트 29 〉	통과 (125.66ms, 54.7MB)
+테스트 30 〉	통과 (93.15ms, 44.6MB)
+테스트 31 〉	통과 (102.91ms, 52.5MB)
+테스트 32 〉	통과 (82.28ms, 47.5MB)
+
+"""
