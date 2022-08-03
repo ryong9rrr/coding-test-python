89db5446f1d032196334f0c9c8e61d43ab09cd56
yong <tkddbs1644@gmail.com>
2022-08-03 13:58:39 +0900

    프로그래머스Lv2: 기능개발

--- 자료구조_큐, 우선순위 큐/프로그래머스lv2-기능개발.js
+++ 자료구조_큐, 우선순위 큐/프로그래머스lv2-기능개발.js
@@ -7,12 +7,12 @@ function solution(progresses, speeds) {
   let prev = q.shift()
   let result = [1]
   while (q.length > 0) {
-    const x = q.shift()
-    if (prev >= x) {
+    const current = q.shift()
+    if (prev >= current) {
       result[result.length - 1]++
     } else {
       result.push(1)
-      prev = x
+      prev = current
     }
   }
   return result
@@ -30,36 +30,109 @@ function solution(progresses, speeds) {
 // 테스트 10 〉	통과 (0.10ms, 30.2MB)
 // 테스트 11 〉	통과 (0.08ms, 30.3MB)
 
-// 큐를 뒤집어서 스택처럼 사용했을 때
+// 스택을 큐처럼 사용했을 때
 function solution(progresses, speeds) {
-  const q = []
-  for (let i = 0; i < speeds.length; i++) {
-    q.push(Math.ceil((100 - progresses[i]) / speeds[i]))
+  const N = progresses.length
+  const stack = []
+  for (let i = 0; i < N; i++) {
+    stack.push(Math.ceil((100 - progresses[i]) / speeds[i]))
   }
-  q.reverse() // shift 연산을 사용하지 않기 위해
-  let prev = q.pop()
+  stack.reverse() // 스택을 큐 처럼 사용하기 위해
+  let prev = stack.pop()
   let result = [1]
-  while (q.length > 0) {
-    const x = q.pop()
-    if (prev >= x) {
+  while (stack.length > 0) {
+    const current = stack.pop()
+    if (prev >= current) {
       result[result.length - 1]++
     } else {
       result.push(1)
-      prev = x
+      prev = current
     }
   }
   return result
 }
-
 // 정확성  테스트
-// 테스트 1 〉	통과 (0.07ms, 30.1MB)
-// 테스트 2 〉	통과 (0.11ms, 30.3MB)
-// 테스트 3 〉	통과 (0.11ms, 30.4MB)
-// 테스트 4 〉	통과 (0.12ms, 30.4MB)
-// 테스트 5 〉	통과 (0.09ms, 30.2MB)
-// 테스트 6 〉	통과 (0.07ms, 30.2MB)
-// 테스트 7 〉	통과 (0.10ms, 30MB)
-// 테스트 8 〉	통과 (0.09ms, 30.3MB)
-// 테스트 9 〉	통과 (0.08ms, 30.5MB)
-// 테스트 10 〉	통과 (0.09ms, 30.3MB)
+// 테스트 1 〉	통과 (0.07ms, 30.2MB)
+// 테스트 2 〉	통과 (0.11ms, 30MB)
+// 테스트 3 〉	통과 (0.12ms, 30.1MB)
+// 테스트 4 〉	통과 (0.09ms, 30.1MB)
+// 테스트 5 〉	통과 (0.07ms, 30.1MB)
+// 테스트 6 〉	통과 (0.07ms, 30.1MB)
+// 테스트 7 〉	통과 (0.11ms, 29.9MB)
+// 테스트 8 〉	통과 (0.10ms, 30.2MB)
+// 테스트 9 〉	통과 (0.09ms, 30.1MB)
+// 테스트 10 〉	통과 (0.11ms, 30.1MB)
 // 테스트 11 〉	통과 (0.06ms, 30.1MB)
+
+// Queue 모듈을 구현했을 때 => 아무래도 스택처럼 푼 것보다는 시간이 더 걸릴수밖에 없음.
+// 반드시 "큐"를 사용하여야 하는 문제에서만 써먹어보자.
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = this.tail = null
+    this.size = 0
+  }
+
+  get peek() {
+    return (this.front && this.front.value) || null
+  }
+
+  enqueue(newValue) {
+    const newNode = new Node(newValue)
+    if (!this.front) {
+      this.front = this.tail = newNode
+    } else {
+      this.tail = this.tail.next = newNode
+    }
+    this.size++
+  }
+
+  dequeue() {
+    if (!this.front) return null
+    const extracted = this.front.value
+    this.front = this.front.next
+    this.size--
+    return extracted
+  }
+}
+
+function solution(progresses, speeds) {
+  const N = progresses.length
+  const q = new Queue()
+  for (let i = 0; i < N; i++) {
+    q.enqueue(Math.ceil((100 - progresses[i]) / speeds[i]))
+  }
+
+  const result = [1]
+  let prev = q.dequeue()
+
+  while (q.size > 0) {
+    const current = q.dequeue()
+    if (prev >= current) {
+      result[result.length - 1]++
+    } else {
+      result.push(1)
+      prev = current
+    }
+  }
+
+  return result
+}
+// 정확성  테스트
+// 테스트 1 〉	통과 (0.34ms, 30.2MB)
+// 테스트 2 〉	통과 (0.20ms, 29.9MB)
+// 테스트 3 〉	통과 (0.26ms, 30.1MB)
+// 테스트 4 〉	통과 (0.18ms, 30.1MB)
+// 테스트 5 〉	통과 (0.32ms, 30.2MB)
+// 테스트 6 〉	통과 (0.15ms, 29.9MB)
+// 테스트 7 〉	통과 (0.33ms, 30.1MB)
+// 테스트 8 〉	통과 (0.18ms, 30.1MB)
+// 테스트 9 〉	통과 (0.37ms, 29.9MB)
+// 테스트 10 〉	통과 (0.34ms, 29.9MB)
+// 테스트 11 〉	통과 (0.31ms, 30MB)
--- 자료구조_큐, 우선순위 큐/프로그래머스lv2-기능개발.py
+++ 자료구조_큐, 우선순위 큐/프로그래머스lv2-기능개발.py
@@ -58,3 +58,38 @@ def solution(progresses, speeds):
         if not q:
             result.append(count)
     return result
+
+
+# 22년 7월 풀이
+import math
+from collections import deque
+def solution(progresses, speeds):
+    N = len(progresses)    
+    q = deque([math.ceil((100 - progresses[i]) / speeds[i]) for i in range(N)])
+    
+    result = [1]
+    prev = q.popleft()
+    
+    while q:
+        current = q.popleft()
+        if prev >= current:
+            result[-1] += 1
+        else:
+            result.append(1)
+            prev = current
+    
+    return result
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.1MB)
+테스트 2 〉	통과 (0.04ms, 10.3MB)
+테스트 3 〉	통과 (0.03ms, 10.1MB)
+테스트 4 〉	통과 (0.02ms, 10.1MB)
+테스트 5 〉	통과 (0.01ms, 10.3MB)
+테스트 6 〉	통과 (0.01ms, 10.3MB)
+테스트 7 〉	통과 (0.03ms, 10.1MB)
+테스트 8 〉	통과 (0.01ms, 10.2MB)
+테스트 9 〉	통과 (0.02ms, 10.2MB)
+테스트 10 〉	통과 (0.03ms, 9.98MB)
+테스트 11 〉	통과 (0.01ms, 10.1MB)
+"""
