bafea762d92f7d01f0e5267464882119cff2b492
yong <tkddbs1644@gmail.com>
2022-08-23 17:56:23 +0900

    프로그래머스: Lv1.지갑문제(최소직사각형), Lv2.큰수만들기

--- greedy/프로그래머스lv2-큰 수 만들기.py
+++ greedy/프로그래머스lv2-큰 수 만들기.py
@@ -36,3 +36,38 @@ def solution(number, k):
 테스트 11 〉	통과 (0.01ms, 10.3MB)
 테스트 12 〉	통과 (0.01ms, 10.3MB)
 """
+
+from collections import deque
+def solution(number, k):
+    N = len(number)
+    q = deque(number)
+    stack = deque()
+    
+    while q and k:
+        x = q.popleft()
+        while k and stack and stack[-1] < x:
+            stack.pop()
+            k -= 1
+
+        stack.append(x)
+    
+    while stack and k:
+        stack.pop()
+        k -= 1
+    
+    return "".join(stack + q)
+"""
+정확성 테스트
+테스트 1 〉 통과 (0.01ms, 10.2MB)
+테스트 2 〉 통과 (0.01ms, 10.2MB)
+테스트 3 〉 통과 (0.03ms, 10.1MB)
+테스트 4 〉 통과 (0.06ms, 9.98MB)
+테스트 5 〉 통과 (0.25ms, 10MB)
+테스트 6 〉 통과 (3.99ms, 10.1MB)
+테스트 7 〉 통과 (5.40ms, 10.9MB)
+테스트 8 〉 통과 (13.75ms, 11.2MB)
+테스트 9 〉 통과 (10.70ms, 17.4MB)
+테스트 10 〉 통과 (84.44ms, 16.3MB)
+테스트 11 〉 통과 (0.01ms, 10.1MB)
+테스트 12 〉 통과 (0.02ms, 10.2MB)
+"""
