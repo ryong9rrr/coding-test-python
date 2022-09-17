6a758ca18cbc103e86a657ba3d6f9583b33c7a17
yong <tkddbs1644@gmail.com>
2022-09-17 16:06:46 +0900

    백준3190 - 뱀

--- 구현/백준3190-뱀.py
+++ 구현/백준3190-뱀.py
@@ -1,116 +1,56 @@
-# while 반복문을 이용한 풀이
-from collections import deque
 import sys
-input = lambda:sys.stdin.readline().rstrip()
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
 
-n = int(input())
-# n x n 행렬 (사과가 있는 곳은 1)
-matrix = [[0] * (n + 1) for _ in range(n + 1)]
-k = int(input())
-for _ in range(k):
-    x, y = map(int, input().split())
-    matrix[x][y] = 1
-l = int(input())
-# 방향 회전 정보
-info = []
-for _ in range(l):
+# 입력받기
+N = int(input())
+K = int(input())
+# 편의상 N + 1로 해서 (N + 1) x (N + 1) 행렬 생성
+matrix = [[0] * (N + 1) for _ in range(N + 1)]
+for _ in range(K):
+    r, c = map(int, input().split())
+    # 사과가 있는 곳은 1로 표시
+    matrix[r][c] = 1
+L = int(input())
+# 앞에서 부터 하나하나 뺄 것이므로, x가 증가하는대로 주어지므로 정렬은 할 필요 없다.
+infos = deque()
+for _ in range(L):
     x, c = input().split()
-    info.append((int(x), c))
+    infos.append((int(x), c))
 
+# for row in matrix:
+#     print(row)
+
+# 처음에는 동쪽(오른쪽)으로 이동하므로 동 남 서 북
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-def turn(direction:int, c:str)->int:
+def turn(direction, c):
     if c == "L":
-        direction = (direction - 1) % 4
+        return (direction - 1) % 4
+    return (direction + 1) % 4
+
+cur_x = cur_y = 1
+matrix[cur_x][cur_y] = 2
+direction = time = 0
+snakes = deque()
+snakes.append([cur_x, cur_y])
+
+while True:
+    next_x = cur_x + dx[direction]
+    next_y = cur_y + dy[direction]
+    if 1 <= next_x and next_x <= N and 1 <= next_y and next_y <= N and matrix[next_x][next_y] != 2:
+        if matrix[next_x][next_y] == 0:
+            prev_x, prev_y = snakes.popleft()
+            matrix[prev_x][prev_y] = 0
+        matrix[next_x][next_y] = 2
+        snakes.append([next_x, next_y])
     else:
-        direction = (direction + 1) % 4
-    return direction
+        break
+    time += 1
+    cur_x, cur_y = next_x, next_y
+    if infos and time == infos[0][0]:
+        direction = turn(direction, infos[0][1])
+        infos.popleft()
 
-def simulate():
-    x, y = 1, 1
-    matrix[x][y] = 2
-    direction = 0
-    time = 0
-    index = 0
-    q = deque()
-    q.append((x, y))
-    while True:
-        nx, ny = x + dx[direction], y + dy[direction]
-        if 1 <= nx <= n and 1 <= ny <= n and matrix[nx][ny] != 2:
-            if matrix[nx][ny] == 0:
-                matrix[nx][ny] = 2
-                q.append((nx, ny))
-                px, py = q.popleft()
-                matrix[px][py] = 0
-            elif matrix[nx][ny] == 1:
-                matrix[nx][ny] = 2
-                q.append((nx, ny))
-        else:
-            time += 1
-            break
-        x, y = nx, ny
-        time += 1
-        if index < l and time == info[index][0]:
-            direction = turn(direction, info[index][1])
-            index += 1
-    return time
-
-print(simulate())
-
-
-################# 코드를 조금 짧게
-from collections import deque
-import sys
-input = lambda:sys.stdin.readline().rstrip()
-
-n = int(input())
-# n x n 행렬 (사과가 있는 곳은 1)
-matrix = [[0] * (n + 1) for _ in range(n + 1)]
-k = int(input())
-for _ in range(k):
-    x, y = map(int, input().split())
-    matrix[x][y] = 1
-l = int(input())
-# 방향 회전 정보
-info = []
-for _ in range(l):
-    x, c = input().split()
-    info.append((int(x), c))
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-def turn(direction:int, c:str)->int:
-    if c == "L":
-        direction = (direction - 1) % 4
-    else:
-        direction = (direction + 1) % 4
-    return direction
-
-def simulate():
-    x, y = 1, 1
-    matrix[x][y] = 2
-    direction = time = index = 0
-    q = deque()
-    q.append((x, y))
-    while True:
-        nx, ny = x + dx[direction], y + dy[direction]
-        if 1 <= nx <= n and 1 <= ny <= n and matrix[nx][ny] != 2:
-            if matrix[nx][ny] == 0:
-                px, py = q.popleft()
-                matrix[px][py] = 0
-                q.append((nx, ny))
-            elif matrix[nx][ny] == 1:
-                q.append((nx, ny))
-            matrix[nx][ny] = 2
-        else:
-            time += 1
-            return time
-        time += 1
-        if index < l and time == info[index][0]:
-            direction = turn(direction, info[index][1])
-            index += 1
-        x, y = nx, ny
-
-print(simulate())
+print(time + 1)
