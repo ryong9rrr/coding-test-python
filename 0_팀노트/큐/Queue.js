c786d8086490d0c0432be51baf2327f2f7d36298
yong <tkddbs1644@gmail.com>
2023-06-15 09:50:19 +0900

    큐 인터페이스 변경

--- 0_팀노트/큐/Queue.js
+++ 0_팀노트/큐/Queue.js
@@ -1,51 +1,41 @@
 class MyNode {
   constructor(value) {
-    this.value = value
-    this.next = null
+    this.value = value;
+    this.next = null;
   }
 }
 
 class MyQueue {
   constructor(array = []) {
-    this.front = this.tail = null
-    this.size = 0
+    this.front = this.tail = null;
+    this.length = 0;
 
     for (const el of array) {
-      this.enqueue(el)
+      this.push(el);
     }
   }
 
-  get peek() {
-    return !this.front || !this.tail ? undefined : this.front.value
+  get head() {
+    return !this.front || !this.tail ? undefined : this.front.value;
   }
 
-  get desc() {
-    const result = []
-    let node = this.front
-    while (node) {
-      result.push(node.value)
-      node = node.next
-    }
-    return result.join(" ")
-  }
-
-  enqueue(value) {
-    const node = new MyNode(value)
+  push(value) {
+    const node = new MyNode(value);
     if (!this.front) {
-      this.front = this.tail = node
+      this.front = this.tail = node;
     } else {
-      this.tail = this.tail.next = node
+      this.tail = this.tail.next = node;
     }
-    this.size += 1
+    this.length += 1;
   }
 
-  dequeue() {
+  shift() {
     if (!this.front) {
-      return undefined
+      return undefined;
     }
-    const result = this.front.value
-    this.front = this.front.next
-    this.size -= 1
-    return result
+    const result = this.front.value;
+    this.front = this.front.next;
+    this.length -= 1;
+    return result;
   }
 }
