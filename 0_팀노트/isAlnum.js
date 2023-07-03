0009215aadc53ce302c8292f3661c457eda17425
yong <tkddbs1644@gmail.com>
2023-07-03 12:52:57 +0900

    7/3

--- 0_팀노트/isAlnum.js
+++ 0_팀노트/isAlnum.js
@@ -1,29 +1,22 @@
-function isAlpha(string) {
-  const reg = /[a-zA-Z]/g
-  return string.replace(reg, '') === ''
+function isAlpha(strings) {
+  const reg = new RegExp(/^[a-zA-Z]+$/, 'g');
+  return reg.test(strings);
 }
 
 function isDigit(string) {
   for (const char of string) {
     if (char < '0' || char > '9') {
-      return false
+      return false;
     }
   }
-  return true
+  return true;
 }
 
 function isNumber(string) {
-  return !Number.isNaN(Number(string))
+  return !Number.isNaN(Number(string));
 }
 
 function isAlnum(string) {
-  const reg = /[a-zA-Z0-9]/g
-  return string.replace(reg, '') === ''
+  const reg = /[a-zA-Z0-9]/g;
+  return string.replace(reg, '') === '';
 }
-
-const string = '1231a'
-
-console.log(isAlpha(string))
-console.log(isDigit(string))
-console.log(isAlnum(string))
-console.log(isNumber(string))
--- 0_팀노트/순열과조합/permute.js
+++ 0_팀노트/순열과조합/permute.js
@@ -1,31 +1,31 @@
 // nums: int[], k: int -> int[][]
 function permute(array, k) {
-  if (k > array.length) return null
-  const results = []
-  const prevElements = []
+  if (k > array.length) return [];
+  const results = [];
+  const prevElements = [];
   function dfs(elements, k) {
     if (k === 0) {
-      results.push([...prevElements])
-      return
+      results.push([...prevElements]);
+      return;
     }
 
     for (let i = 0; i < elements.length; i++) {
-      const nextElements = [...elements]
-      nextElements.splice(i, 1)
+      const nextElements = [...elements];
+      nextElements.splice(i, 1);
 
-      prevElements.push(elements[i])
-      dfs(nextElements, k - 1)
-      prevElements.pop()
+      prevElements.push(elements[i]);
+      dfs(nextElements, k - 1);
+      prevElements.pop();
     }
   }
-  dfs(array, k)
-  return results
+  dfs(array, k);
+  return results;
 }
 
-const nums = [1, 2, 3]
+const nums = [1, 2, 3];
 
 for (let i = 1; i <= 3; i++) {
-  console.log(permute(nums, i))
+  console.log(permute(nums, i));
 }
 
 /*
