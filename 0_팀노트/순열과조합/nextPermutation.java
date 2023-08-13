e34c8173c5e82b76b851e5ec4259c91887b5f1c7
yong <tkddbs1644@gmail.com>
2023-08-14 00:03:26 +0900

    Next-Permutation 보충

--- 0_팀노트/순열과조합/nextPermutation.java
+++ 0_팀노트/순열과조합/nextPermutation.java
@@ -1,11 +1,22 @@
 import java.io.*;
 import java.util.*;
 
+// 끝이 있는 Next-Permutation ([3, 2, 1] 다음 순열부터는 구하지 않음)
 public class Solution {
     public static void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i += 1;
+            j -= 1;
+        }
     }
 
     public static boolean nextPermutation(int[] nums){
@@ -31,13 +42,7 @@ public class Solution {
         swap(nums, i - 1, j);
 
         // 역으로 스왑하면서 재정렬
-        int k = nums.length - 1;
-        while (i < k) {
-            swap(nums, i, k);
-            i += 1;
-            k -= 1;
-        }
-
+        reverse(nums, i);
         return true;
     }
 
@@ -48,5 +53,49 @@ public class Solution {
         while (nextPermutation(nums)) {
             System.out.println(Arrays.toString(nums));
         }
+    }
+}
+
+// 끝이 없는 순열 ([3, 2, 1] 다음 순열은 [1, 2, 3])
+class Solution {
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start){
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i += 1;
+            j -= 1;
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        
+        // 뒤에있는 수가 앞에있는 수보다 같거나 작을때까지 인덱스 찾기
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i -= 1;
+        }
+
+        // 뒤에서부터 다시 탐색
+        if (i >= 0) {
+            int j = nums.length - 1;
+            
+            // 뒤에있는 수가 앞에있는 수보다 같거나 작을때까지 인덱스 찾기
+            while (nums[j] <= nums[i]) {
+                j -= 1;
+            }
+
+            // 찾았으니 스왑
+            swap(nums, i, j);
+        }
+
+        // 바꾼 수열의 뒷부분을 역정렬
+        reverse(nums, i + 1);
     }
 }
